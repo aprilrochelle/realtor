08396732d4676b6c4e3ce4703865acbbb7fed533
April Nichols <nicholsar@gmail.com>
2018-06-26 19:15:08 -0500

    ListingForm component

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
+import ListingForm from '../components/ListingForm/ListingForm';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div className="App">
         <Listings />
         <Building />
+        <ListingForm />
       </div>
     );
   }
