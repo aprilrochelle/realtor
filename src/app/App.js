2cf0354f798689cd2527fb597c1de507e3d7ad9a
April Nichols <nicholsar@gmail.com>
2018-06-26 21:36:14 -0500

    listing prices showing

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import connection from '../firebaseRequests/connection';
+import listingRequests from '../firebaseRequests/listings';
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
@@ -8,14 +9,26 @@ import ListingForm from '../components/ListingForm/ListingForm';
 import './App.css';
 
 class App extends Component {
+  state = {
+    listings: [],
+  }
+
   componentDidMount () {
     connection();
+    listingRequests.getRequest()
+      .then((listings) => {
+        this.setState({listings});
+      })
+      .catch((err) => {
+        console.error('error with listing GET', err);
+      });
   }
+
   render () {
     return (
       <div className="App">
         <div className="col-md-6">
-          <Listings />
+          <Listings listings={this.state.listings}/>
         </div>
         <div className="col-md-6">
           <Building />
