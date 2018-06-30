6343a34e55ded7deb273917bfb6bb8cac61c487f
April Nichols <nicholsar@gmail.com>
2018-06-30 11:20:24 -0500

    listing item component

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import {listingShape} from '../../propz/listingProp';
 import './Listings.css';
 
+import ListingItem from '../ListingItem/ListingItem';
+
 class Listings extends React.Component {
   static propTypes = {
     listings: PropTypes.arrayOf(listingShape),
@@ -11,7 +13,7 @@ class Listings extends React.Component {
     const { listings } = this.props;
     const listingsItemComponents = listings.map((listing) => {
       return (
-        <li key={listing.id}>{listing.price}</li>
+        <ListingItem listing={listing} key={listing.id}/>
       );
     });
 
